45afcee94282f28c42de68c81b3bdcfae1e2f16b
Fernando Tong <nandorov@gmail.com>
2016-05-07 10:02:01 -0500

    Add New Route UI initial sketch

--- app/src/main/java/com/belatrix/pickmeup/EmptyActivity.java
+++ app/src/main/java/com/belatrix/pickmeup/EmptyActivity.java
@@ -1,13 +1,28 @@
 package com.belatrix.pickmeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EmptyActivity extends AppCompatActivity {
+
+    private Button addRouteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty);
+
+        addRouteBtn = (Button)findViewById(R.id.add_route_btn);
+
+        addRouteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EmptyActivity.this, AddRoute.class);
+                startActivity(intent);
+            }
+        });
     }
 }
