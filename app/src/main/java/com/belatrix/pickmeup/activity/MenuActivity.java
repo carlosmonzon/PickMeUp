e94e797ae7846e4b2a6010f79dbdb86380b4b56a
RoggerTipiani <blinker06@gmail.com>
2016-05-13 18:11:22 -0500

    creating activity menu file

---
+++ app/src/main/java/com/belatrix/pickmeup/activity/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.belatrix.pickmeup.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.belatrix.pickmeup.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
