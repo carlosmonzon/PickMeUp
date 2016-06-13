c8a4e46309d6fe70079ee1b247a4d97423ec6d26
Angel Tapara <angel.tapara@teamaol.com>
2016-06-13 12:08:52 -0400

    PageDetail screen

--- app/src/main/java/com/belatrix/pickmeup/activity/EmptyActivity.java
+++ app/src/main/java/com/belatrix/pickmeup/activity/EmptyActivity.java
@@ -12,6 +12,8 @@ public class EmptyActivity extends AppCompatActivity {
 
     private Button addRouteBtn;
 
+    private Button showPageDetail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,15 @@ public class EmptyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(EmptyActivity.this, AddRouteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        showPageDetail = (Button) findViewById(R.id.page_detail_btn);
+        showPageDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EmptyActivity.this, DetailPageActivity.class);
                 startActivity(intent);
             }
         });
